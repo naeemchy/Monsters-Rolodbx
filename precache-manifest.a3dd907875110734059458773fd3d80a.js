0c7a826c4f5d703ae5b572f6ff0ae679da4e08d1
Naeem Uddin <naeem.u.md@gmail.com>
2020-07-06 14:46:41 +0600

    Updates

---
+++ precache-manifest.a3dd907875110734059458773fd3d80a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fdb72466ed8b98cc3ec16da5dcde7f9",
+    "url": "/naeemchy/Monsters-Rolodbx/index.html"
+  },
+  {
+    "revision": "e54bb90278fca0e86191",
+    "url": "/naeemchy/Monsters-Rolodbx/static/css/main.74c69466.chunk.css"
+  },
+  {
+    "revision": "f757cf264434f883c100",
+    "url": "/naeemchy/Monsters-Rolodbx/static/js/2.fcb612a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/naeemchy/Monsters-Rolodbx/static/js/2.fcb612a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e54bb90278fca0e86191",
+    "url": "/naeemchy/Monsters-Rolodbx/static/js/main.7ab141cd.chunk.js"
+  },
+  {
+    "revision": "0384004f7da525e46638",
+    "url": "/naeemchy/Monsters-Rolodbx/static/js/runtime-main.12cd2ba2.js"
+  }
+]);
